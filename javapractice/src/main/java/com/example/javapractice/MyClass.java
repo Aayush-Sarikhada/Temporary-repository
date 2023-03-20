55ec7779f0b94e1e8bba80bbef5dac4537ffcb16
Aayush Sarikhada <aayush.s@simformsolutions.com>
2023-03-20 14:11:22 +0530

    init

---
+++ javapractice/src/main/java/com/example/javapractice/MyClass.java
@@ -0,0 +1,28 @@
+package com.example.javapractice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MyClass {
+
+
+        interface somaIcha {
+            int a = 0;
+        }
+
+        class done implements somaIcha {
+
+        }
+
+        static List<String> arrayListOfString = Arrays.asList("a b c d e f g".split(" "));
+        static Map<Integer,Integer> mapOfInts = new HashMap<Integer,Integer>();
+
+        public static void main(String[] args) {
+
+        }
+}
